580a70c1ff12feefa981f54dc3ca6b69821f660d
Kalana Heshan <148562579+kalana250@users.noreply.github.com>
2024-06-09 22:30:46 +0530

    Add files via upload

---
+++ Java_Mysql_Db_Connection/src/Java_Mysql_Database_Connection.java
@@ -0,0 +1,51 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mcs
+ */
+public class Java_Mysql_Database_Connection {
+   
+    public static Connection c;
+    
+    static {
+    
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3307/DatabaseName";
+            String un = "root";
+            String pw = "kalana@123";
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3307/DatabaseName?useSSL=false", un, pw);
+            
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    
+    }
+    public static void iud(String query) throws Exception{
+    
+        Statement s = c.createStatement();
+        s.executeUpdate(query);
+        
+    }
+    
+    public static ResultSet search(String query) throws Exception{
+    
+        Statement s = c.createStatement();
+        ResultSet rs = s.executeQuery(query);
+        return rs;
+        
+    }
+    
+}
